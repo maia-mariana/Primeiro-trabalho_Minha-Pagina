ccc830d3c8b2d5a59f15baa395f8c065be61f241
maia-mariana <mari.rmaia23@gmail.com>
2026-05-25 13:57:20 -0300

    Add files via upload

---
+++ Contador/contador.js
@@ -0,0 +1,37 @@
+let valorAtual = 0;
+const btnIncremento = document.querySelector("#btn-mais");
+const btnDecremento = document.querySelector("#btn-menos");
+const btnReset = document.querySelector("#btn-reset");
+
+btnIncremento.addEventListener("click", () => {
+  valorAtual++;
+  atualizaValor(valorAtual);
+  atualizaMensagem("Valor incrementado com sucesso");
+});
+
+btnDecremento.addEventListener("click", () => {
+  if (valorAtual > 0) {
+    valorAtual--;
+    atualizaValor(valorAtual);
+    atualizaMensagem("Valor decrementado com sucesso");
+  } else {
+    atualizaMensagem("If protegeu para não ter valor negativo");
+  }
+});
+
+btnReset.addEventListener("click", () => {
+  valorAtual = 0;
+  atualizaValor(valorAtual);
+  atualizaMensagem("Valor resetado com sucesso");
+});
+
+function atualizaValor(valor) {
+  const display = document.querySelector("#valor");
+  display.textContent = valor;
+}
+
+function atualizaMensagem(texto) {
+  const mensagem = document.querySelector("#mensagem");
+  // CORRIGIDO: de elementoMensagem para mensagem
+  mensagem.textContent = texto; 
+}
